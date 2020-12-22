da8332805c3a89516f140b745ee3d6030fb00cbd
MacBookPro <matthewpalmer9@outlook.com>
2020-12-21 23:12:53 -0600

    Added the Fast JSON api, set up users and sessions to allow creation of accounts and logging in/out.

--- Gemfile
+++ Gemfile
@@ -19,11 +19,16 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+gem 'jwt'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
+
+# Usee fast-json-api
+gem 'fast_jsonapi'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,10 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
+    skip_before_action :verify_authenticity_token
+    helper_method :current_user
 
-    def encode_token(payload)
-        JWT.encode(payload, 's3cr3t')
-    end 
-
-    def auth_header
-        request.headers['Authorization'] 
-    end
-
-    def decode_token
-        if auth_header
-                token = auth_header.split(' ')[1]
-            begin
-                JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
-            rescue JWT::DecodeError
-                nil
-            end
+    def current_user
+        if session[:user_id]
+            @current_user = User.find(session[:user_id])
         end 
-    end
-
-    def logged_in_user
-        if decode_token
-            user_id = decoded_token[0]['user_id']
-            @user = User.find_by(id: user_id)
-        end 
-    end
-    
-    def logged_in?
-        !!logged_in_user
     end 
-
-    def authorized
-        render json: { message: 'Please log in'}, status: :unauthorized unless logged_in?
-    end
 end
