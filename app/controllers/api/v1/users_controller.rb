ba6e415aa7adcad680ea2064c3a06df59ef9cb97
MacBookPro <matthewpalmer9@outlook.com>
2021-01-25 15:17:42 -0600

    Changed the method for acquiring a user list by also including all list items associated with that user.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,14 +5,24 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
+            user.username.downcase
             @token = issue_token(user)
             list = List.create(name: user.username)
             list.user_id = user.id
+            user.save
             list.save
             render json: { user: UserSerializer.new(user), jwt: @token }, status: :created 
         else 
             render json: { error: user.errors.full_messages }, status: :not_acceptable
         end 
+    end 
+
+    def get_user_list
+        user = User.find_by(username: params["user"])
+        list = List.find_by(user_id: user.id)
+        listitems = Listitem.where(user_id: user.id)
+        
+        render json: { user: user, list: list, listitems: listitems }
     end 
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/users', to: 'users#create'
+      post '/users/public/list', to: 'users#get_user_list'
       post '/users/:id/list', to: 'lists#get_list'
-      post '/users/:id/list/add', to: 'listitems#create'
+      post '/users/list/add', to: 'listitems#create'
       post '/login', to: 'auth#create'
       get '/current_session', to: 'auth#show'
     end
