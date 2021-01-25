80f01b262042fac8feefadc27c50094c73201e44
MacBookPro <matthewpalmer9@outlook.com>
2021-01-25 12:35:55 -0600

    Added a table to display user list data.

--- app/controllers/api/v1/listitems_controller.rb
+++ app/controllers/api/v1/listitems_controller.rb
@@ -1,3 +1,18 @@
 class Api::V1::ListitemsController < ApplicationController
 
+    def create 
+        user = User.find_by(id: user_id)
+        list = List.find_by(user_id: user.id)
+        
+        listitem = Listitem.create(list_params)
+        listitem.list_id = list.id 
+        listitem.user_id = user.id
+        listitem.save
+    end 
+
+    private
+
+    def list_params
+        params.require(:list).permit(:description, :url)
+    end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/users', to: 'users#create'
       post '/users/:id/list', to: 'lists#get_list'
+      post '/users/:id/list/add', to: 'listitems#create'
       post '/login', to: 'auth#create'
       get '/current_session', to: 'auth#show'
     end
